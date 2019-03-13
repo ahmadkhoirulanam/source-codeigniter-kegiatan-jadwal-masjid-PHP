63bd6ad9eed34ec225e4907dd3696b621287a5a3
Anam Khoirul <kanam5999@gmail.com>
2019-03-13 17:37:28 +0700

    update #1

---
+++ application/views/template/footer/footer-copyright.php
@@ -0,0 +1,7 @@
+<li>
+<center style="color:#FFFFFF;font-size:10px;margin:5px;">
+Copyright 2019. 
+<br>
+<span style="font-size:8px;">Template by : <a href="">Anam-Adit</a></span>
+</center>
+</li>
---
+++ application/views/template/home.php
@@ -0,0 +1,8 @@
+<div class="row well">
+<h3><i class="fa fa-th-large fa-fw"></i> <?php echo $this->config->item('website_name', 'tank_auth'); ?> Web</h3>
+<hr>
+Masjid (bentuk tidak baku: mesjid) adalah rumah tempat ibadah umat Islam atau Muslim. Masjid artinya tempat sujud, dan sebutan lain bagi masjid di Indonesia adalah musholla, langgar atau surau. Istilah tersebut diperuntukkan bagi masjid yang tidak digunakan untuk Sholat Jum'at, dan umumnya berukuran kecil. Selain digunakan sebagai tempat ibadah, masjid juga merupakan pusat kehidupan komunitas muslim. Kegiatan-kegiatan perayaan hari besar, diskusi, kajian agama, ceramah dan belajar Al Qur'an sering dilaksanakan di Masjid. Bahkan dalam sejarah Islam, masjid turut memegang peranan dalam aktivitas sosial kemasyarakatan hingga kemiliteran. <br><br>
+Semua muslim yang telah baligh atau dewasa diperintahkan untuk menunaikan sholat wajib lima kali sehari secara berjamaah di masjid kecuali ada halangan. Walaupun beberapa masjid hanya dibuka pada hari Jumat, tetapi masjid yang lainnya menjadi tempat sholat sehari-hari. Pada hari Jumat, semua muslim laki-laki yang telah dewasa diwajibkan pergi ke masjid untuk menunaikan sholat Jum'at selama tidak ada halangan, berdasarkan Surah Al-Jumu’ah ayat 9: <br><br>
+Wahai orang-orang yang beriman! Apabila telah diseru untuk melaksanakan salat Jum'at, maka bersegeralah kamu mengingat Allah dan tinggalkanlah jual beli. Yang demikian itu lebih baik bagimu jika kamu mengetahui.
+.
+</div>
